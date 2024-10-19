ad3f776eaddf03f377dc07377a3358f81890c596
Ethan Young <PicardY24@gmail.com>
2024-10-19 16:30:46 -0400

    import fbx and manip armature and mesh

--- model/model_creation.py
+++ model/model_creation.py
@@ -1,12 +1,27 @@
 import bpy
 import os
 
-# Ensure the scene is clear (optional)
-bpy.ops.object.select_all(action='SELECT')
-bpy.ops.object.delete()
+def load_model():
+    # Ensure the scene is clear (optional)
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete()
 
-# get human model file path
-obj_file_path = os.path.join(os.path.dirname(__file__), "obj", "body_model_with_landmarks.obj")
+    # get human model file path
+    fbx_file_path = os.path.join(os.path.dirname(__file__), "obj", "scene.fbx")
 
-# Import the .obj file
-bpy.ops.wm.obj_import(filepath=obj_file_path)
+    # Import the .obj file
+    bpy.ops.import_scene.fbx(filepath=fbx_file_path)
+
+    # Access the armature and mesh objects
+    armature = bpy.data.objects["Armature"]  # Replace with the actual armature name
+    mesh = bpy.data.objects["body_model_with_landmarks"]  # Replace with the actual mesh name
+
+    # Print their scales to check if they match
+    print(f"Armature scale: {armature.scale}")
+    print(f"Mesh scale: {mesh.scale}")
+
+
+def animate():
+    pass
+
+load_model()
