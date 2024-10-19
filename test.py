8ca20713d66a186b4ae2109b7926443a698e2868
rahulmedicharla <rmedicharla@gmail.com>
2024-10-19 14:31:11 -0400

    added test sript

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pose.pose as pose
 import depth.depth as depth
 import preprocess.preprocess as preprocess
+from pose import draw
 
 def main():
     FILE_PATH = "test_data/walk.mp4"
@@ -11,7 +12,7 @@ def main():
     cap = cv2.VideoCapture(FILE_PATH)
 
     all_landmarks = []
-    all_distance_map
+    all_distance_map = []
 
     while True:
         ret, frame = cap.read()
@@ -24,10 +25,12 @@ def main():
 
         print("getting pose landmarks")
         landmarks = pose.get_landmarks(cropped_image, True)
-
         
-        print("getting depth")
-        landmarks = depth.inference_image(cropped_image, debug, landmarks)
+        annotated_img = np.copy(landmarks)
+        for keypoint in landmarks:
+            cv2.circle(annotated_img, (landmarks[keypoint][0], landmarks[keypoint][1]), 5, (0, 255, 0), -1)
+
+        all_landmarks.append(annotated_img)
 
     cap.release()
 
